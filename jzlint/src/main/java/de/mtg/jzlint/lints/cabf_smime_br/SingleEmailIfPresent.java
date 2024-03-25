f28acad7d0688aa64d3baaff96050935c9c91d47
mtgag <githreg@mtg.de>
2024-03-25 07:38:55 +0100

    zlint PR 802, 808

--- jzlint/src/main/java/de/mtg/jzlint/lints/cabf_smime_br/SingleEmailIfPresent.java
+++ jzlint/src/main/java/de/mtg/jzlint/lints/cabf_smime_br/SingleEmailIfPresent.java
@@ -13,10 +13,28 @@ import de.mtg.jzlint.Status;
 import de.mtg.jzlint.utils.SMIMEUtils;
 import de.mtg.jzlint.utils.Utils;
 
+
+/*************************************************************************
+ 7.1.4.2.1 Subject alternative name extension
+
+ All Mailbox Addresses in the subject field or entries of type dirName of this extension SHALL be
+ repeated as rfc822Name or otherName values of type id-on-SmtpUTF8Mailbox in this
+ extension.
+
+ 7.1.4.2.2 Subject distinguished name fields
+
+ h. Certificate Field: subject:emailAddress (1.2.840.113549.1.9.1) Contents: If present, the
+ subject:emailAddress SHALL contain a single Mailbox Address as verified under
+ Section 3.2.2.
+
+ Combining these requirements, this lint checks for malformed email addresses in SAN entries
+ covering the case of a non-single Mailbox Address.
+ *************************************************************************/
+
 @Lint(
         name = "e_single_email_if_present",
-        description = "If present, the subject:emailAddress SHALL contain a single Mailbox Address",
-        citation = "7.1.4.2.h",
+        description = "If present, the subject:emailAddress SHALL contain a single Mailbox Address. All Mailbox Addresses in the subject field SHALL be repeated as rfc822Name or otherName values of type id-on-SmtpUTF8Mailbox in SAN extension.",
+        citation = "7.1.4.2.1 and 7.1.4.2.2.h",
         source = Source.CABF_SMIME_BASELINE_REQUIREMENTS,
         effectiveDate = EffectiveDate.SMIME_BR_1_0_DATE)
 public class SingleEmailIfPresent implements JavaLint {
@@ -29,7 +47,7 @@ public class SingleEmailIfPresent implements JavaLint {
 
             for (String email : emails) {
                 if (!SMIMEUtils.isValidEmailAddress(email)) {
-                    return LintResult.of(Status.ERROR, String.format("subject:emailAddress was present and contained an invalid email address (%s)", email));
+                    return LintResult.of(Status.ERROR, String.format("san:emailAddress was present and contained an invalid email address (%s)", email));
                 }
             }
             return LintResult.of(Status.PASS);
